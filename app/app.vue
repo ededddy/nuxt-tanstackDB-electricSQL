3c929e21c249287a16cd63dbfccd14971a1fafbc
ededddy <eddylei070300@gmail.com>
2026-01-15 14:47:29 +0800

    feat: handle ssr stuff, allow collection to preload

--- app/app.vue
+++ app/app.vue
@@ -2,6 +2,10 @@
 import { useLiveQuery } from "@tanstack/vue-db";
 import { todosCollection } from "~~/lib/collections";
 
+onMounted(async () => {
+    await todosCollection.preload();
+});
+
 const { data } = useLiveQuery(q =>
     q.from({ todos: todosCollection })
         .select(({ todos }) => ({
